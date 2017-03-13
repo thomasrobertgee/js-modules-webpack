2e0e82f2b8758684e1fe3f4501419dbac41018a3
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-13 17:46:35 +1100

    moving on from basic of webpack to more complex and interesting parts like codesplitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path') // a built in module that helps with the path for the file output
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const config = {
   entry: './src/index.js', // the entry point
   output: {
     path: path.resolve(__dirname, 'build'), // where the file output will be outputted to
-    filename: 'bundle.js' // name of file
+    filename: 'bundle.js', // name of file
+    publicPath: 'build/'
   },
   module: {
     rules: [
@@ -13,12 +15,26 @@ const config = {
         test: /\.js$/ // a regular expression to apply babel to any files ending with .js
       },
       {
-        use: ['style-loader', 'css-loader'],
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
         test: /\.css$/
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 } // 40,000 byte limit
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
-  }
-
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 }
 
 module.exports = config
